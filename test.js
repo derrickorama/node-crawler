63e1c2e2f5474ab2da4148e66c1730d1981b51ba
Derrick Gall <derrick.gall@thomsonreuters.com>
2017-04-19 11:41:26 -0500

    Fixed up test script to accept command line arguments (for url) and output redirects
    - also added yarn setup

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
-var Crawler = require('./crawler').Crawler;
+#!/usr/bin/env node
+/* eslint no-console:0 */
 
-var crawler = new Crawler({
+const argv = require('yargs')
+  .usage('Usage: $0 [url]')
+  .demandCommand(1)
+  .argv;
+const _ = require('lodash');
+const Crawler = require('./crawler').Crawler;
+
+const crawler = new Crawler({
   excludes: [
     /\/mt-search\.cgi/g
   ],
@@ -8,31 +16,37 @@ var crawler = new Crawler({
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
     'Accept-Encoding': 'gzip, sdch',
     'Accept-Language': 'en-US,en;q=0.8',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.76 Safari/537.36',
+    'User-Agent': `Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36${''
+      }(KHTML, like Gecko) Chrome/41.0.2272.76 Safari/537.36`,
     'PageSpeed': 'off'
   },
   workers: 10
 });
 
-crawler.start('http://www.attwatersjamesonhill.co.uk');
-crawler.on('pageCrawled', function (response, body) {
-  console.log(pagesCrawled() + '|' + pagesLeft());
+const pagesCrawled = () => {
+  return crawler.get('urlsCrawled').length;
+};
+
+const pagesLeft = () => {
+  return crawler.get('urlsQueued').length;
+};
+
+crawler.start(_.first(argv._));
+crawler.on('pageCrawled', (/*response, body*/) => {
+  console.log(`${pagesCrawled()} crawled | ${pagesLeft()} left`);
 });
-crawler.on('error', function (err, response) {
+crawler.on('redirect', (response) => {
+  if (!response.isExternal) {
+    console.log(`Redirect: ${response.redirect} -> ${response.url}`);
+  }
+});
+crawler.on('error', (err, response) => {
   console.error('-->');
-  console.error('Erorr on: ' + response.url);
-  console.error(err ? err.stack : 'Bad status: ' + response.statusCode);
+  console.error(`Erorr on: ${response.url}`);
+  console.error(err ? err.stack : `Bad status: ${response.statusCode}`);
   console.error('Referrer:', response.referrer);
-  console.log(pagesCrawled() + '/' + pagesLeft());
+  console.log(`${pagesCrawled()}/${pagesLeft()}`);
 });
-crawler.on('finish', function () {
+crawler.on('finish', () => {
   console.log('done');
 });
-
-function pagesCrawled() {
-  return crawler.get('urlsCrawled').length;
-}
-
-function pagesLeft() {
-  return crawler.get('urlsQueued').length;
-}
